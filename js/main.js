ede0a980642dea9313337807318df7855d2900d9
Glebario <works.suhovoy@gmail.com>
2019-07-16 02:17:57 +0300

    PhotoPost№2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var photos = 25;
 
-//--------------------------------------масив с коментариями-------------------------------
+// масив с коментариями
 var commentsArray = [
     'Всё отлично!',
     'В целом всё неплохо. Но не всё.',
@@ -10,7 +10,7 @@ var commentsArray = [
     'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-//--------------------------------------масив с именами-------------------------------
+// масив с именами
 var namesArray = [
     'ВИКТОР',
     'ОЛЬГА',
@@ -20,7 +20,7 @@ var namesArray = [
     'ДАНИИЛ'
 ];
 
-//--------------------------------------масив с svg-------------------------------
+// масив с svg
 var svgArray = [
     'avatar-1.svg',
     'avatar-2.svg',
@@ -30,85 +30,78 @@ var svgArray = [
     'avatar-6.svg'
 ];
 
-
-//------------------------функция генерации рандомного числа-------------------------------
-function getRandomInRangeNamber(min, max){
-
+// функция генерации рандомного числа
+function getRandomInRangeNamber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+}
 
-
-
-//--------------------------------------рандомное svg(аватарка)-------------------------------
-var randomNamberSvg = getRandomInRangeNamber(1, svgArray.length);
-var randomSvg = svgArray[randomNamberSvg];
-
-
-
-//--------------------------------------рандомное имя пользователя-------------------------------
-var randomNamberName = getRandomInRangeNamber(1, namesArray.length);
-var randomName = namesArray[randomNamberName];
-
-
-
-//--------------------------------------рандомное кол-во лайков-------------------------------
-var randomNamberLikes = getRandomInRangeNamber(15, 200);
-console.info(randomNamberLikes);
-
-//----------------------рандомное кол-во коментариев под одной фото------------------------
-var message = function(){
-    message = [];
+// Генерация рандомных коментарий(message)
+function message(){
+    messageArray = [];
     for(var i = 0; i < 2; i++){
        var randomNamberComments = getRandomInRangeNamber(1, commentsArray.length);
        var randomComments = commentsArray[randomNamberComments];
-       message[i] = randomComments;
+       messageArray[i] = randomComments;
     };
-   return message;
-};
-//console.info(message());
+   return messageArray;
+}
 
 
+// Масив коментарий
+function getComments(count) {
+    var comments = [];
 
-//--------------------------------------генерация обьектов(фото) с разными данными---------- 
-function generationObject(index, randomNamberLikes, randomSvg, randomName, message){
+    for(var i = 0; i < count; i++) {
+        comments.push({
+             avatar: "img/" + randomSvg,
+             message: message(),
+             name: randomName
+        });
+    };
+    return comments;
+}
+
+// генерация обьектов(фото) с разными данными
+function generationObject(index, randomNamberLikes, commentsArray) {
     var photoArray = {
         url: 'photos/' + index + '.jpg',
         likes: randomNamberLikes,
-        comments: {
-            avatar: 'img/' + randomSvg,
-            message: message,
-            name: randomName,
-        }
+        comments: commentsArray
     };
     return photoArray;  
-};
+}
 
 for(var i = 0; i < photos; i++) {
-    //-------------номер url, likes,comments--------------------------------------------
-    var photoPost = generationObject(i + 1 , getRandomInRangeNamber(15, 200) , 
-    svgArray[getRandomInRangeNamber(1, svgArray.length)] ,
-     namesArray[getRandomInRangeNamber(1, namesArray.length)] , message);
-    console.info(photoPost);
+    // рандомное svg(аватарка
+    var randomNamberSvg = getRandomInRangeNamber(1, svgArray.length);
+    var randomSvg = svgArray[randomNamberSvg];
 
+    // рандомное имя пользователя
+    var randomNamberName = getRandomInRangeNamber(1, namesArray.length);
+    var randomName = namesArray[randomNamberName];
 
-    //--------------------------------------находим шаблон-------------------------------
+    // рандомное кол-во лайков
+    var randomNamberLikes = getRandomInRangeNamber(15, 200);
+
+    // номер url, likes,comments
+    var photoPost = generationObject(i + 1 , randomNamberLikes , getComments(getRandomInRangeNamber(1, 20)));
+
+    // находим шаблон
     var teamplateUser = document.querySelector('#picture').content.querySelector('.picture');
-    //--------------------------------------копируем шаблон-------------------------------
+
+    // копируем шаблон
     var photoElement = teamplateUser.cloneNode(true);
 
-
-
-    //--------------------подстовляем данные в скопированные шаблоны-----------------------------
+    // подстовляем данные в скопированные шаблоны
     photoElement.querySelector('.picture__img').setAttribute('src', photoPost.url);
     photoElement.querySelector('.picture__likes').textContent = photoPost.likes;
-    photoElement.querySelector('.picture__comments').textContent = photoPost.comments;
-    //console.info(photoElement);
-
-
+    photoElement.querySelector('.picture__comments').textContent = photoPost.comments.length;
+    console.info(photoElement);
 
     var fragment = document.createDocumentFragment();
     fragment.appendChild(photoElement);
-    //------------------------подстовляем заполненные шаблоны на сайт-------------------------------
+
+    // подстовляем заполненные шаблоны на сайт
     var pictures = document.querySelector('.pictures');
     pictures.appendChild(fragment);
 };
