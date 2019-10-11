ff3c4ec7323a50666ab05c31749b46bf13ff43de
Glebario <works.suhovoy@gmail.com>
2019-10-12 01:34:03 +0300

    Просмотр фотопоста в увеличенном режиме

--- js/main-screen.js
+++ js/main-screen.js
@@ -1,128 +1,5 @@
 (function () {
 
-/*
-//--------------------------------------ЗАГРУЗКА ФОТО СО СТОТИЧЕСКИМИ ДАННЫМИ--------------------------------
-var photos = 25;
-
-// находим шаблон
-var teamplateUser = document.querySelector('#picture').content.querySelector('.picture');
-
-// масив с коментариями
-var commentsArray = [
-    'Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
-
-// масив с именами
-var namesArray = [
-    'ВИКТОР',
-    'ОЛЬГА',
-    'ОЛЕКСАНДР',
-    'АНАСТАСИЯ',
-    'СВЕТЛАНА',
-    'ДАНИИЛ'
-];
-
-// масив с svg
-var svgArray = [
-    'avatar-1.svg',
-    'avatar-2.svg',
-    'avatar-3.svg',
-    'avatar-4.svg',
-    'avatar-5.svg',
-    'avatar-6.svg'
-];
-
-// функция генерации рандомного числа
-function getRandomInRangeNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// Генерация рандомных коментарий(message)
-function generateCommentMessage() {
-    messageArray = [];
-
-    for(var i = 0; i < 2; i++){
-       var randomNumberComments = getRandomInRangeNumber(1, commentsArray.length);
-       var randomComments = commentsArray[randomNumberComments];
-       messageArray[i] = randomComments;
-    };
-   return messageArray;
-}
-
-// Масив коментарий
-function getPhotoPostComments(count) {
-    var comments = [];
-
-    for(var i = 0; i < count; i++) {
-        
-        // рандомное svg(аватарка)
-        var randomNumberUserSvg = getRandomInRangeNumber(1, svgArray.length);
-        var userAvatarImageName = svgArray[randomNumberUserSvg];
-
-        // рандомное имя пользователя
-        var randomNumberUserName = getRandomInRangeNumber(1, namesArray.length);
-        var userName = namesArray[randomNumberUserName];
-
-        comments.push({
-             avatar: "img/" + userAvatarImageName,
-             message: generateCommentMessage(),
-             name: userName
-        });
-    };
-    return comments;
-}
-
-// генерация обьектов(фото) с разными данными
-function generatePhotoPostObject(numberUserUrlAdress, quantityLikes, comments) {
-    var photoArray = {
-        url: 'photos/' + numberUserUrlAdress + '.jpg',
-        likes: quantityLikes,
-        comments: comments
-    };
-    return photoArray;  
-}
-
-for(var i = 0; i < photos; i++) {
-    // подстовляем значения url, likes, comments
-    var photoPost = generatePhotoPostObject(i + 1 , photoPostLikes , getPhotoPostComments(getRandomInRangeNumber(1, 20)));
-    console.info(photoPost);
-
-    // рандомное кол-во лайков
-    var photoPostLikes = getRandomInRangeNumber(15, 200);
-
-    addDataToCopiedTemplates();
-    addTemplatesToSite();
-};
-
-// подстовляем данные в скопированные шаблоны
-function addDataToCopiedTemplates() {
-    // копируем шаблон
-    var photoElement = teamplateUser.cloneNode(true);
-
-    photoElement.querySelector('.picture__img').setAttribute('src', photoPost.url);
-    photoElement.querySelector('.picture__likes').textContent = photoPost.likes;
-    photoElement.querySelector('.picture__comments').textContent = photoPost.comments.length;
-
-    return photoElement;
-}
-
-// подстовляем заполненные шаблоны на сайт
-function addTemplatesToSite() {
-
-    var fragment = document.createDocumentFragment();
-    fragment.appendChild(addDataToCopiedTemplates());
-
-    var pictures = document.querySelector('.pictures');
-    pictures.appendChild(fragment);
-
-    return fragment
-}
-*/
 
 //-------------------------------------------ЗАГРУЗКА ФОТО С СЕРВЕРА-----------------------------
 
@@ -138,6 +15,18 @@ function addDataToCopiedTemplates(data) {
     photoElement.querySelector('.picture__likes').textContent = data.likes;
     photoElement.querySelector('.picture__comments').textContent = data.comments.length;
 
+    //событие дляпросмотра фото вувеличенном масштабе
+    photoElement.addEventListener('click', function(){
+        window.viewFotopostBigScale.BigPicture(data);
+    });
+    window.btnCloseBigPicture.addEventListener('click', function(){
+        window.viewFotopostBigScale.closeBigPicture();
+    });
+    window.fotoPostBigPicture.addEventListener('keydown', function(evt){
+        if (evt.keyCode == 27) {
+            window.viewFotopostBigScale.closeBigPicture();
+        };
+    });
     return photoElement;
 };
   
@@ -199,7 +88,6 @@ function highlight(btn) {
 //удаление постов(фото) при смене фильтра
 function removePostCard(){
     var postContainer = document.querySelector('.pictures');
-
     //console.info(postContainer);
     while(postContainer.children[2]){
         //console.info(postContainer.children[2]);
@@ -214,13 +102,12 @@ var successHandlerNew = function (data) {
     //console.log(data);
     var fragment = document.createDocumentFragment();
     
-    var filtered = data.sort(function(){
+    var filteredNew = data.sort(function(){
         return Math.random() - 0.5;
     }).slice(0, 10);
-    //console.log(filtered);
 
-    for (var i = 0; i < filtered.length; i++) {
-    fragment.appendChild(addDataToCopiedTemplates(filtered[i]));
+    for (var i = 0; i < filteredNew.length; i++) {
+    fragment.appendChild(addDataToCopiedTemplates(filteredNew[i]));
     };
 
     var pictures = document.querySelector('.pictures');
@@ -230,7 +117,9 @@ var successHandlerNew = function (data) {
 //обсуждаемые посты
 var successHandlerDiscussed = function (data) {
     var fragment = document.createDocumentFragment();
-    var filtered = data.sort(function(a, b){
+
+    //сортировка по возростанию
+    var filteredDiscussed = data.sort(function(a, b){
         var c = a.comments,
             d = b.comments;
     
@@ -242,12 +131,8 @@ var successHandlerDiscussed = function (data) {
         return 0;
     }).reverse();
     
-    for(var i in data){
-        console.log(data[i]); 
-    };
-    
-    for (var i = 0; i < filtered.length; i++) {
-    fragment.appendChild(addDataToCopiedTemplates(filtered[i]));
+    for (var i = 0; i < filteredDiscussed.length; i++) {
+    fragment.appendChild(addDataToCopiedTemplates(filteredDiscussed[i]));
     };
 
     var pictures = document.querySelector('.pictures');
@@ -256,18 +141,16 @@ var successHandlerDiscussed = function (data) {
 
 //обработчики событий фильтров
 
-    filterPopular.addEventListener('click', function(event){
-        removePostCard();
-        window.load(url, successHandler, errorHandler);
-    })
-    filterNew.addEventListener('click', function(event){
-        removePostCard();
-        window.load(url, successHandlerNew, errorHandler);
-    })
-    filterDiscussed.addEventListener('click', function(event){
-        removePostCard();
-        window.load(url, successHandlerDiscussed, errorHandler);
-    })
-
+filterPopular.addEventListener('click', function(event){
+    removePostCard();
+    window.load(url, successHandler, errorHandler);
+});
+filterNew.addEventListener('click', function(event){
+    removePostCard();
+    window.load(url, successHandlerNew, errorHandler);
+});
+filterDiscussed.addEventListener('click', function(event){
+    removePostCard();
+    window.load(url, successHandlerDiscussed, errorHandler);
+});
 })();
- 
